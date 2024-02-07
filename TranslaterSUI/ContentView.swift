3435a8219d558e51083e09cf3a359665301e6fe5
SF-testUmnyasha <newashedelo@gmail.com>
2024-02-07 10:10:10 +0200

    Initial Commit

---
+++ TranslaterSUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TranslaterSUI
+//
+//  Created by Oleg Arnaut  on 07.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TranslaterSUI/TranslaterSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslaterSUIApp.swift
+//  TranslaterSUI
+//
+//  Created by Oleg Arnaut  on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TranslaterSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
